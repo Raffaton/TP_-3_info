38df88312cdfeb7dc2d30f95111f55838b93eba7
Arthur <arthur.pocholle@gmail.com>
2024-09-19 13:04:49 +0200

    personages

--- ex1.c
+++ ex1.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 int main()
 {
-  printf("Hello");
-  printf("Coucou grosse merde");
+  char personages[3][100]={" ", " "," "};
+  
+  srand(time(NULL));
+  for (int i = 0; i < 3; i++)
+  {
+
+    printf("Entrer un nom de personnages : ");
+    fgets(personages[i],sizeof(personages[i]),stdin);
+
+    
+  }
+  
+  int aleatoire=rand()%3;
+  char choix_personage[100];
+  strcpy(choix_personage,personages[aleatoire]);
+
+  if (choix_personage==)
+  {
+    /* code */
+  }
+  
+  
+  
+
+
+  
+  
   return 0;
 }
